ae546805b64c5ac72cc842bf86dfa8ef50465ea2
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-03-23 18:48:23 +0530

    chore: basic setup

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useRef } from "react";
+
+export default function App() {
+  const videoRef = useRef(null);
+
+  const startCamera = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    videoRef.current.srcObject = stream;
+  }
+
+  return (
+    <div>
+      <h1>Zoom Clone Day 1</h1>
+
+      <video ref={videoRef} autoPlay playsInline style={{ width: "500px" }} />
+
+      <button onClick={startCamera}>Start Camera</button>
+    </div>
+  );
+}
