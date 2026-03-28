fbd904b86712f727542d39d2dadec74fce9a51f5
Vinayak Majhi <vinayakmajhi2002@gmail.com>
2026-03-28 20:55:45 +0530

    feat: refactor

--- server/server.js
+++ server/server.js
@@ -16,19 +16,23 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("User connected");
 
-  socket.on("offer", (data) => {
-    socket.broadcast.emit("answer", data);
+  socket.on("offer", (offer) => {
+    socket.broadcast.emit("offer", offer);
   });
 
-  socket.on("answer", (data) => {
-    socket.broadcast.emit("answer", data);
+  socket.on("answer", (answer) => {
+    socket.broadcast.emit("answer", answer);
   });
 
-  socket.on("ice-candidate", (data) => {
-    socket.broadcast.emit("ice-candidate", data);
+  socket.on("ice-candidate", (candidate) => {
+    socket.broadcast.emit("ice-candidate", candidate);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User left");
   });
 });
 
 server.listen(5000, () => {
-  console.log("Server running");
+  console.log("Server running 5000");
 });
